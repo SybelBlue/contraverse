5c2b3d0f68c66ea99d0cbba9cbb0c636c548f468
winstongergill <winstongergill612@gmail.com>
2018-04-07 16:25:35 -0500

    stuff happened

--- app/src/main/java/homi/sybelblue/contraversev12/UserDBHandler.java
+++ app/src/main/java/homi/sybelblue/contraversev12/UserDBHandler.java
@@ -6,33 +6,74 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
 import android.content.ContentValues;
 
-public class UserDBHandler extends SQLiteOpenHelper{
+public class UserDBHandler extends SQLiteOpenHelper {
+    private final int NUM_TOPICS;
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "Users";
     private static final String TABLE_NAME = "Users";
     private static final String COLUMN_ID = "ID";
+    private static final String COLUMN_NAME = "Name";
     private static final String COLUMN_SS = "SS";
     private static final String COLUMN_SF = "SF";
     private static final String COLUMN_FS = "FS";
     private static final String COLUMN_FF = "FF";
 
-    public UserDBHandler(Context context, SQLiteDatabase.CursorFactory factory){
+    public UserDBHandler(Context context, SQLiteDatabase.CursorFactory factory, int numTopics) {
         super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+        NUM_TOPICS = numTopics;
     }
 
-    public String getTableString(){
-        String result = "";
+    public String getTableString() {
+        StringBuilder result = new StringBuilder();
         String query = "SELECT*FROM " + TABLE_NAME;
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor cursor = db.rawQuery(query, null);
-        return result;
+        while (cursor.moveToNext()) {
+            int result_0 = cursor.getInt(0);
+            String result_1 = cursor.getString(1);
+            result.append(String.valueOf(result_0));
+            result.append(" ");
+            result.append(result_1);
+            result.append(System.getProperty("line.separator"));
+        }
+        cursor.close();
+        db.close();
+        return result.toString();
+    }
+
+    public void addUser(User user) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_ID, user.ID);
+        values.put(COLUMN_NAME, user.name);
+        values.put(COLUMN_SS, user.getSS());
+        values.put(COLUMN_SF, user.getSF());
+        values.put(COLUMN_FS, user.getFS());
+        values.put(COLUMN_FF, user.getFF());
+        for (int i = 0; i < NUM_TOPICS; i++) {
+            values.put("Topics" + i, user.topics.get(i).toString());
+        }
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "(" + COLUMN_ID + "INTEGER PRIMARY KEY," + COLUMN_SS +
-                "INTEGER," + COLUMN_SF + "INTEGER," + COLUMN_FS + "INTEGER," + COLUMN_FF + "INTEGER)";
-        db.execSQL(CREATE_TABLE);
+        StringBuilder CREATE_USER_TOPICS = new StringBuilder("CREATE TABLE " + "Topics" + "(");
+        StringBuilder CREATE_USERS_TABLE = new StringBuilder("CREATE TABLE " + TABLE_NAME + "(" + COLUMN_ID +
+                "INTEGER PRIMARY KEY," + COLUMN_NAME + "TEXT" + COLUMN_SS +
+                "INTEGER," + COLUMN_SF + "INTEGER," + COLUMN_FS + "INTEGER," + COLUMN_FF + "INTEGER)");
+
+        for (int i = 0; i < NUM_TOPICS; i++) {
+            CREATE_USER_TOPICS.append("Topic");
+            CREATE_USERS_TABLE.append("Topic");
+            CREATE_USER_TOPICS.append(i);
+            CREATE_USERS_TABLE.append(i);
+            CREATE_USER_TOPICS.append("TEXT,");
+            CREATE_USERS_TABLE.append("INTEGER,");
+        }
+
+        CREATE_USER_TOPICS.replace(CREATE_USER_TOPICS.length() - 1, CREATE_USER_TOPICS.length() - 1, "");
+        CREATE_USERS_TABLE.replace(CREATE_USERS_TABLE.length() - 1, CREATE_USERS_TABLE.length() - 1, "");
+        db.execSQL(CREATE_USERS_TABLE.toString());
+        db.execSQL(CREATE_USER_TOPICS.toString());
     }
 
     @Override
