1d0bcf8c3d58b4a23b9b8805b0fc844be9fc526d
Keara Berlin <kberlin@macalester.edu>
2018-04-07 17:17:56 -0500

    Tried to add LoginActivity, but it doesn't do anything yet. I deleted Logan's test radio buttons because they broke it.

--- app/src/main/java/homi/sybelblue/contraversev12/MainActivity.java
+++ app/src/main/java/homi/sybelblue/contraversev12/MainActivity.java
@@ -1,5 +1,7 @@
 package homi.sybelblue.contraversev12;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -17,17 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.buttonTest);
-        button.setOnClickListener(v -> toastRelay("Hi!"));
-
-        RadioButton radioButton = findViewById(R.id.testRadio0);
-        radioButton.setOnCheckedChangeListener(this::defaultOnCheckedChanged);
-
-        radioButton = findViewById(R.id.testRadio1);
-        radioButton.setOnCheckedChangeListener(this::defaultOnCheckedChanged);
-
-        Switch aSwitch = findViewById(R.id.switch2);
-        aSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> doToast = isChecked);
+        // Check whether the user has set up their "account" yet
+        SharedPreferences preferences = getSharedPreferences("", 0);
+        int userID = preferences.getInt(getString(R.string.user_id_pref_key), -1); // i = -1 means the default return val is -1
+        // If they have not, start the user profile setup activity
+        if(userID == -1){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        // if they have, get the user's data from our database
+        else{
+            //TODO get user data from database
+        }
     }
 
     private void defaultOnCheckedChanged(CompoundButton buttonView, boolean isChecked) {
