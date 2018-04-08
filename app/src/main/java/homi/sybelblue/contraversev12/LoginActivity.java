afa8d5c8fabdde657ddd2399fc8efc9bc1a1289f
Keara Berlin <kberlin@macalester.edu>
2018-04-07 21:33:09 -0500

    Put login activity into two separate activities so it's nicer. Hasn't been tested yet.

--- app/src/main/java/homi/sybelblue/contraversev12/LoginActivity.java
+++ app/src/main/java/homi/sybelblue/contraversev12/LoginActivity.java
@@ -1,27 +1,19 @@
 package homi.sybelblue.contraversev12;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import java.util.prefs.Preferences;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ConstraintLayout confirmationLayout;
     private LinearLayout form;
 
     private EditText displayNameView;
 
-    private TextView confirmationText;
-
-    private String displayName;
+    public String displayName;
 
 
     @Override
@@ -30,9 +22,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
 
         // set onClickListeners for all buttons
-        findViewById(R.id.login_back).setOnClickListener(this);
         findViewById(R.id.login_next).setOnClickListener(this);
-        findViewById(R.id.login_submit).setOnClickListener(this);
     }
 
     @Override
@@ -40,14 +30,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onStart();
 
         displayNameView = findViewById(R.id.login_disp_name);
-        confirmationLayout = findViewById(R.id.login_confirmation);
-        confirmationText = findViewById(R.id.login_confirmation_text);
         form = findViewById(R.id.login_form);
-
-        // make the form visible so user can answer the questions; make the confirmation text invisible
-        //TODO make these separate activities so it isn't a fucking monster
-        form.setVisibility(View.VISIBLE);
-        confirmationLayout.setVisibility(View.GONE);
 
         //TODO actually ask the startup questions (I think they are officially called SpecificQuestion Questions)
     }
@@ -57,31 +40,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()){
             case R.id.login_next:
                 displayName = displayNameView.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), LoginConfActivity.class);
+                intent.putExtra("displayName", displayName);
+                startActivity(intent);
                 //TODO get the responses for the rest of the startup questions
-
-                confirmationLayout.setVisibility(View.VISIBLE);
-                confirmationText.setText(displayName);
                 break;
 
-            case R.id.login_back:
-                confirmationLayout.setVisibility(View.GONE);
-                form.setVisibility(View.VISIBLE);
-                break;
-
-            case R.id.login_submit:
-                // save the user's ID in the sharedpreferences
-                //TODO generate an actual user ID????
-                SharedPreferences preferences = getSharedPreferences(getString(R.string.preferences_filename), 0);
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putInt(getString(R.string.user_id_pref_key), 3); //TODO store the data in the database
-                editor.commit();
-
-                User user = new User(3, new int[]{0});
-                user.name = displayName;
-
-                MainActivity.userDBHandler.addUser(user);
-
-                finish();
         }
     }
 
---
+++ app/src/main/java/homi/sybelblue/contraversev12/LoginConfActivity.java
@@ -0,0 +1,55 @@
+package homi.sybelblue.contraversev12;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginConfActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView confirmationText;
+    private String displayName;
+    private Intent startIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_conf);
+
+        findViewById(R.id.login_submit).setOnClickListener(this);
+        startIntent = getIntent();
+        displayName = startIntent.getStringExtra("displayName");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        confirmationText = findViewById(R.id.login_confirmation_text);
+    }
+
+    @Override
+    public void onClick(View v){
+
+        switch (v.getId()){
+            case R.id.login_submit:
+                // save the user's ID in the sharedpreferences
+                //TODO generate an actual user ID????
+                SharedPreferences preferences = getSharedPreferences(getString(R.string.preferences_filename), 0);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt(getString(R.string.user_id_pref_key), 3); //TODO store the data in the database
+                editor.commit();
+
+                User user = new User(3, new int[]{0});
+                user.name = displayName;
+
+                MainActivity.userDBHandler.addUser(user);
+
+                finish();
+        }
+
+    }
+
+}
