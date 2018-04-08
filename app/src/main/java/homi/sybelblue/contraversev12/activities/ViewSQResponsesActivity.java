1b5297178772f00853718771634a68f606e42271
Keara Berlin <kberlin@macalester.edu>
2018-04-08 06:19:26 -0500

    Added ViewSQResponsesActivity and initialized in Manifest

---
+++ app/src/main/java/homi/sybelblue/contraversev12/activities/ViewSQResponsesActivity.java
@@ -0,0 +1,15 @@
+package homi.sybelblue.contraversev12.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import homi.sybelblue.contraversev12.R;
+
+public class ViewSQResponsesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_sqresponses);
+    }
+}
